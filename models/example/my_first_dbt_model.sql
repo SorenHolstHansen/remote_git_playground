09b174aa9cc8f3bc9e21ef86c8f3bad028ece20a
Admin Jensen <admin@wayfare.ai>
2024-10-16 14:51:00 +0200

    fsdgdf

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -3,7 +3,7 @@ with sub as (
         blns_dataset._wayfare_updated_at as _wayfare_updated_at,
         blns_dataset.blns_col as blns_col,
         blns_dataset._wayfare_created_at as _wayfare_created_at
-    from {{ source('dummy_sync', 'blns_dataset') }} as blns_dataset
+    from {{ source('test', 'blns_dataset') }} as blns_dataset
 )
 
 select
